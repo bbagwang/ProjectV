ad60eabc53bff2380d1f07b5ae768b625d301fc8
BBAGWANG <bbagwang@gmail.com>
2025-09-20 22:48:22 +0900

    Add VHUD Class

---
+++ Source/ProjectV/UI/VHUD.h
@@ -0,0 +1,15 @@
+// Copyright BBAGWANG (GyuTae Hong). All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "VHUD.generated.h"
+
+
+UCLASS()
+class PROJECTV_API AVHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
