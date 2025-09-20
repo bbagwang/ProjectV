0d242b778c6ee0e46a543f4dccb8b0308ff6c454
BBAGWANG <bbagwang@gmail.com>
2025-09-20 22:47:50 +0900

    Add VPlayerController Class

---
+++ Source/ProjectV/Character/VPlayerController.h
@@ -0,0 +1,15 @@
+// Copyright BBAGWANG (GyuTae Hong). All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "VPlayerController.generated.h"
+
+
+UCLASS()
+class PROJECTV_API AVPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
